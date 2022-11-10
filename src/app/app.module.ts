aaaa63e8a6c1cf70f1288c382b52fc50f8647678
Archit Jain <architjee@gmail.com>
2022-11-10 19:11:24 +0530

    Installed the scully library and added it to the imports of app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { ContactModule } from './contact/contact.module';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
-
+import {ScullyLibModule } from '@scullyio/ng-lib'
 @NgModule({
   declarations: [
     AppComponent
@@ -16,7 +16,8 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     CoreModule,
     SharedModule,
-    ContactModule
+    ContactModule,
+    ScullyLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
