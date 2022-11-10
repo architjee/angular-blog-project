06ec0cca62adaf4fb98c31cd815872219d7de589
Archit Jain <architjee@gmail.com>
2022-11-10 19:33:59 +0530

    Modified articles component to acomodate scully's needs.

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ScullyRoute , ScullyRoutesService} from '@scullyio/ng-lib'
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.scss']
 })
 export class ArticlesComponent implements OnInit {
-
-  constructor() { }
+posts: ScullyRoute[] = [];
+  constructor(private scullyService: ScullyRoutesService) { }
 
   ngOnInit(): void {
+    this.scullyService.available$.subscribe(posts=>{
+this.posts = posts.filter(post=> post.title);
+    })
   }
 
 }
